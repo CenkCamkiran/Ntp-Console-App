ae6edba3ac543afa53856e77b02d1ff57bda4818
Cenk Camkıran <cenkcamkiran@gmail.com>
2024-05-13 09:55:58 +0300

    commit

---
+++ NtpClient/NtpHelper.cs
@@ -0,0 +1,29 @@
+﻿using GuerrillaNtp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NtpClient
+{
+    public class NtpHelper
+    {
+        public NtpClock QueryWithBackoff(GuerrillaNtp.NtpClient client)
+        {
+            var delay = TimeSpan.FromSeconds(1);
+            while (true)
+            {
+                try
+                {
+                    return client.Query();
+                }
+                catch
+                {
+                    Thread.Sleep(delay);
+                    delay = 2 * delay;
+                }
+            }
+        }
+    }
+}
---
+++ NtpClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using GuerrillaNtp;
+using NtpClient;
+
+GuerrillaNtp.NtpClient client = GuerrillaNtp.NtpClient.Default;
+NtpHelper ntpHelper = new NtpHelper();
+
+while (true)
+{
+    NtpClock clock = ntpHelper.QueryWithBackoff(client); // see example above
+
+    DateTimeOffset local = clock.Now;
+    DateTimeOffset utc = clock.UtcNow;
+
+    DateTime localDate = clock.Now.LocalDateTime;
+    DateTime utcDate = clock.UtcNow.UtcDateTime;
+
+    Console.WriteLine("Local Now Date: " + local);
+    Console.WriteLine("Utc Now Date: " + utc);
+    Console.WriteLine("LocalDate Now Date: " + localDate);
+    Console.WriteLine("UtcDate Now Date: " + utcDate);
+
+    Thread.Sleep(TimeSpan.FromDays(1));
+}
